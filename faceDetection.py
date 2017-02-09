25905d6554b16282d5792a4994fc5bf745af9a7e
eiab30p <everde@rollins.edu>
2017-02-09 18:53:24 -0500

    Face Detection Back to original

--- faceDetection.py
+++ faceDetection.py
@@ -1,7 +1,6 @@
 import numpy as np
 import cv2
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+
 
 ### https://pythonprogramming.net/haar-cascade-face-eye-detection-python-opencv-tutorial/
 
@@ -10,34 +9,10 @@ face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 
 
-#video = cv2.VideoCapture(0)
-# initialize the camera and grab a reference to the raw camera capture
-camera = PiCamera()
-camera.resolution = (640, 480)
-camera.framerate = 32
-rawCapture = PiRGBArray(camera, size=(640, 480))
+video = cv2.VideoCapture(0)
 
-# allow the camera to warmup
-time.sleep(0.1)
-
-# capture frames from the camera
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=True):
-    # grab the raw NumPy array representing the image, then initialize the timestamp
-    # and occupied/unoccupied text
-    image = frame.array
-
-
-
-while(frame.isOpened()):
-
-    #ret, frame = video.read()
-    # show the frame
-    ret = cv2.imshow("Frame", image)
-    key = cv2.waitKey(1) & 0xFF
-
-    # clear the stream in preparation for the next frame
-    rawCapture.truncate(0)
-
+while(video.isOpened()):
+    ret, frame = video.read()
     if ret:
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         faces = face_cascade.detectMultiScale(gray, 1.5, 1)
@@ -55,7 +30,6 @@ while(frame.isOpened()):
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-rawCapture.truncate(0)
 
-#video.release()
-#cv2.destroyAllWindows()
+video.release()
+cv2.destroyAllWindows()
