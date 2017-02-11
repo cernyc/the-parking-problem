0048aad2ef205075a5447da41bc53a0b2fef51dc
michael <mgutensohn@rollins.edu>
2017-02-11 11:04:24 -0500

    changed frame to image... again

--- piCam.py
+++ piCam.py
@@ -26,7 +26,7 @@ for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port=
     for (x,y,w,h) in faces:
         cv2.rectangle(image, (x, y), (x + w, y + h), (255, 1, 1), 1)
         roi_gray = gray[y:y + h, x:x + w]
-        roi_color = frame[y:y + h, x:x + w]
+        roi_color = image[y:y + h, x:x + w]
         eyes = eye_cascade.detectMultiScale(roi_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (1, 255, 1), 2)
