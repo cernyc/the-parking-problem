55b77a9e41253d0f13e6d2f709771d7d4414767a
eiab30p <everde@rollins.edu>
2017-02-16 18:00:29 -0500

    Updated Image view and new resize image function

--- carDetection.py
+++ carDetection.py
@@ -9,11 +9,13 @@ cars_cascade = cv2.CascadeClassifier('cars.xml')
 img = cv2.imread('pictures/image3.jpg',0)
 cars = cars_cascade.detectMultiScale(img, scaleFactor = 1.1,
                                    minNeighbors = 10, minSize=(200,200),
-                                   flags = cv2.cv.CV_HAAR_SCALE_IMAGE)
+                                   flags = cv2.CASCADE_SCALE_IMAGE)
 for (x,y,w,h) in cars:
     cv2.rectangle(img,(x,y),(x+w,y+h),(255,1,1),1)
 
-    cv2.imshow('cars?', img)
+    resized_image = cv2.resize(img, (700, 700))
+
+    cv2.imshow('cars?', resized_image)
 
 
     if cv2.waitKey(0) & 0xFF == ord('q'):
