eb9bfa9e5f2c6f05d88c3fa9f9187896c403e57e
michael <mgutensohn@rollins.edu>
2017-02-17 15:02:01 -0500

    found new cascade that performs better.

--- carDetection.py
+++ carDetection.py
@@ -3,17 +3,16 @@ import cv2
 import time
 
 
-cars_cascade = cv2.CascadeClassifier('cars.xml')
+cars_cascade = cv2.CascadeClassifier('lbp_cars.xml')
 
 
 img = cv2.imread('pictures/image3.jpg',0)
-cars = cars_cascade.detectMultiScale(img, scaleFactor = 1.1,
-                                   minNeighbors = 10, minSize=(200,200),
-                                   flags = cv2.CASCADE_SCALE_IMAGE)
+cars = cars_cascade.detectMultiScale(img, scaleFactor = 1.2,
+                                   minNeighbors = 5, minSize=(150,150),flags = cv2.CASCADE_SCALE_IMAGE)
 for (x,y,w,h) in cars:
-    cv2.rectangle(img,(x,y),(x+w,y+h),(255,1,1),1)
+    cv2.rectangle(img,(x,y),(x+w,y+h),(255,1,1),3)
 
-    resized_image = cv2.resize(img, (700, 700))
+    resized_image = cv2.resize(img, (960, 540))
 
     cv2.imshow('cars?', resized_image)
 
